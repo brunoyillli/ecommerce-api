a4919b57445b76d3f700538d8c582de0d7cd1f4c
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-04-30 10:31:41 -0300

    configurando cors

--- src/main/java/io/github/brunoyillli/ecommerceapi/repository/ProductRepository.java
+++ src/main/java/io/github/brunoyillli/ecommerceapi/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package io.github.brunoyillli.ecommerceapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import io.github.brunoyillli.ecommerceapi.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
