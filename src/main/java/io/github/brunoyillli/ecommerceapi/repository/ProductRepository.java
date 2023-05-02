42b09d57aedce9f87aea13d8a7cc08e0c6f02149
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-05-02 16:34:42 -0300

    filtrando por categoria

--- src/main/java/io/github/brunoyillli/ecommerceapi/repository/ProductRepository.java
+++ src/main/java/io/github/brunoyillli/ecommerceapi/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package io.github.brunoyillli.ecommerceapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import io.github.brunoyillli.ecommerceapi.entity.Product;
@@ -8,4 +11,5 @@ import io.github.brunoyillli.ecommerceapi.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
