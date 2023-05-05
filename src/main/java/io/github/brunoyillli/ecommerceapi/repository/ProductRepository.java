5b86b9c10ec2ec020a8de865206ea61f1ce11a5b
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-05-04 23:16:14 -0300

    buscndo pelo nome do produto

--- src/main/java/io/github/brunoyillli/ecommerceapi/repository/ProductRepository.java
+++ src/main/java/io/github/brunoyillli/ecommerceapi/repository/ProductRepository.java
@@ -12,4 +12,6 @@ import io.github.brunoyillli.ecommerceapi.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+	
+	Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
