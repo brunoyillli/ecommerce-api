8d296c7c31bf90165f81dc0a8dd72f32f33b8e65
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-06-02 09:41:12 -0300

    order history secure backend

---
+++ src/main/java/io/github/brunoyillli/ecommerceapi/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package io.github.brunoyillli.ecommerceapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import com.okta.spring.boot.oauth.Okta;
+
+@Configuration
+public class SecurityConfiguration {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(configurer -> configurer
+				.antMatchers("/api/orders/**")
+				.authenticated())
+				.oauth2ResourceServer().jwt();
+		http.cors();
+		http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+		Okta.configureResourceServer401ResponseBody(http);
+		return http.build();
+	}
+}
